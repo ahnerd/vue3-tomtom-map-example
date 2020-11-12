376f616be4c95e8de1cc96f4f897aecab0bed976
techiediaries <techiediaries9@gmail.com>
2020-11-12 02:50:21 +0100

    First commit

---
+++ src/components/Map.vue
@@ -0,0 +1,59 @@
+<template>
+    
+    <h1>Vue 3 TomTom Maps Demo</h1>
+    <div id='map' ref="mapRef"></div>
+</template>
+
+<script>
+import { onMounted,  ref } from 'vue'
+
+export default {
+  name: 'Map',
+  setup() {
+    const mapRef = ref(null);
+    onMounted(() => {
+      
+            const tt = window.tt;
+            var map = tt.map({
+            key: 'iTF86GRA2V5iGjM6LMMV54lrK8v6zC1w',
+            container: mapRef.value,
+            style: 'tomtom://vector/1/basic-main',
+        });
+        map.addControl(new tt.FullscreenControl());
+        map.addControl(new tt.NavigationControl()); 
+        addMarker(map);
+
+    })
+
+    function addMarker(map){
+       const tt = window.tt;
+        var location = [-121.91595, 37.36729];
+        var popupOffsets = {
+  top: [0, 0],
+  bottom: [0, -30],
+  'bottom-right': [0, -30],
+  'bottom-left': [0, -30],
+  left: [25, -35],
+  right: [-25, -35]
+}
+
+        var marker = new tt.Marker().setLngLat(location).addTo(map);
+        var popup = new tt.Popup({offset: popupOffsets}).setHTML("Your address!");
+marker.setPopup(popup).togglePopup();
+    }
+
+    return {
+      mapRef,
+    }
+    
+
+  }
+  
+}
+</script>
+<style>
+#map {
+    height: 50vh;
+    width: 50vw;
+}
+</style>
